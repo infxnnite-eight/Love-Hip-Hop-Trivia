d63cef16c3e87f0740c9f8dd6c51b4efdbe6c6f1
June238025$ <dorianquintana@gmail.com>
2023-08-19 15:18:19 -0700

    JS-1-Carnival-Rides

---
+++ class/JavaScript Test.js
@@ -0,0 +1,4 @@
+function hello (Dorian) {
+    console.log ('Hi', Dorian)
+}
+
---
+++ class/JavaScripts Course - Beginners.js
@@ -0,0 +1,13 @@
+ let a = 7; {
+        
+ }
+
+ console.log (a);
+
+
+ function dice (){
+    return Math.floor (math.random()*6+1;)
+
+    console.log (dice)
+
+ }
